ebd59d36f7bdd3d02ca2cf0cc6345271f75e48b3
Eric Buess <ericbuess@gmail.com>
2019-06-03 14:22:18 -0500

    Add parse function skeleton

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 (function() {
 	"use strict"
-	console.log("test");
-	console.warn("test");
-	console.info("test");
+	
+	/* TODO: 
+		Grab url
+		Parse URL into new learning objects
+		Display learning objects
+		Allow user to edit learning objects
+		Render learning objects in 3 UI methods
+	*/
+	
+	function parseURL(url) {
+		var html = 'parsed html';
+		return html;
+	}
+	
+	$("#try-it").click(function() {
+		var url = $("#url").val();
+		console.log(url);
+		var parsed = parseURL(url);
+		console.log("parsed: ", parsed);
+	});
 })()
