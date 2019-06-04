5a807489b0a0f096418b40bc885a550fa2e7a559
Eric Buess <ericbuess@gmail.com>
2019-06-03 19:21:30 -0500

    Add getHTML function placeholder

--- js/main.js
+++ js/main.js
@@ -4,20 +4,28 @@
 	/* TODO: 
 		Grab url
 		Parse URL into new learning objects
+		https://developer.mozilla.org/en-US/docs/Web/API/DOMParser -- compare to jquery
 		Display learning objects
 		Allow user to edit learning objects
 		Render learning objects in 3 UI methods
 	*/
 	
-	function parseURL(url) {
-		var html = 'parsed html';
+	function getHTML(url) {
+		var html = 'some html';
+		return html;
+	}
+	
+	function parseHTML(html) {
+		// do something
 		return html;
 	}
 	
 	$("#try-it").click(function() {
 		var url = $("#url").val();
 		console.log(url);
-		var parsed = parseURL(url);
+		var html = getHTML(url);
+		$("#html").html(html);
+		var parsed = parseHTML(html);
 		console.log("parsed: ", parsed);
 	});
 })()
